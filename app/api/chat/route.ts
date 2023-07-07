e6fec5faf0ad7dded80370932e945eb8da3f2542
Rodney Brown <133543395+rodneymbrown1@users.noreply.github.com>
2023-07-07 13:59:08 -0700

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,7 +3,7 @@ import { OpenAIStream, StreamingTextResponse } from 'ai'
  
 // Create an OpenAI API client (that's edge friendly!)
 const config = new Configuration({
-  apiKey: process.env.OPEN_API_KEY
+  apiKey: process.env.OPENAI_API_KEY
 })
 const openai = new OpenAIApi(config)
 console.log(JSON.stringify(config));
