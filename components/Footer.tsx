fd18f9c301fc825efdb2fbe488473b6dfb58ef46
Rodney Brown <rodney.m.brown1@outlook.com>
2023-07-08 15:11:32 -0700

    ...

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,15 +25,15 @@ export default function Footer() {
        
         <div className="w-full sm:w-1/2 md:w-1/4">
           <h3 className="text-lg font-bold mb-4">Contact</h3>
-          <p className="text-sm">Email: rodney.m.brown1@outlook.com</p>
+          <p className="text-sm">Email: support@devpro.com</p>
           
         </div>
         <div className="w-full sm:w-1/2 md:w-1/4">
           <h3 className="text-lg font-bold mb-4">Connect</h3>
           <ul className="text-sm">
             <li>
-              <a href="https://www.linkedin.com/in/rodney-brown-9319221a6/" target="_blank" rel="noopener noreferrer">
-                LinkedIn
+              <a href="https://twitter.com/DevproHQ" target="_blank" rel="noopener noreferrer">
+                Twitter
               </a>
             </li>
             {/* <li>
